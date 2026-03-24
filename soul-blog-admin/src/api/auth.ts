a7ad655228d73618bbcb8bb7e53ee57aa16ab564
zhuzhujian <670352956@qq.com>
2026-03-25 02:51:43 +0800

    处理接口path

--- soul-blog-admin/src/api/auth.ts
+++ soul-blog-admin/src/api/auth.ts
@@ -3,5 +3,5 @@ import type { IAuthToken, UserInfo } from "@/types/auth";
 import type { IResult } from '@/types/common'
 
 export async function login(userInfo: UserInfo):Promise<IResult<IAuthToken>> {
-  return fetch.post('/api/login', userInfo)
+  return fetch.post('/api/v1/login', userInfo)
 }

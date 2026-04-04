34792c414b00e39f61d54037d84daeae87fdca4b
zhuzhujian <670352956@qq.com>
2026-04-05 02:28:39 +0800

    更新类型定义&添加图片上传记录表

--- scripts/mysql/blog_common.sql
+++ scripts/mysql/blog_common.sql
@@ -13,14 +13,14 @@ CREATE TABLE `article` (
   `desc` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '博客描述',
   `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文章内容（markdown/HTML）',
   `img` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '封面（path/uri）',
-  `type` TINYINT NULL DEFAULT NULL COMMENT '类型（1-AI生成 2-原创 4-转载 8-翻译）',
+  `type` TINYINT NULL DEFAULT NULL COMMENT '类型（0-上传 1-创建 2-AI生成 4-原创 8-转载 16-翻译）',
   `status` TINYINT NULL DEFAULT NULL COMMENT '状态（0-私密 1-公开 2-草稿 4-发布 8-下线）',
   `is_top` TINYINT(1) NULL DEFAULT NULL,
-  `is_delete` TINYINT(1) NULL DEFAULT NULL COMMENT '软珊标识',
+  `is_delete` TINYINT(1) NULL DEFAULT NULL COMMENT '软删标识',
   `create_at` datetime(3) NULL DEFAULT NULL,
   `update_at` datetime(3) NULL DEFAULT NULL,
   `original_url` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '转载原地址',
-  `article_id` VARCHAR(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'uuid',
+  `article_id` BINARY(16) NOT NULL COMMENT 'uuid_to_bin',
   PRIMARY KEY(`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = dynamic;
 
@@ -43,6 +43,8 @@ CREATE TABLE `user_auth`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username`(`username` ASC) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- INSERT INTO `user_auth` VALUES(1, '2026-3-26 23:02:07', '2026-3-26 23:02:07', 'admin', 'admin', '', '', '', '', 0, 1, '');
 
 -- table user_info structure 用户信息
 
@@ -80,6 +82,20 @@ CREATE TABLE `operation_log`  (
   `ip_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作IP',
   `ip_source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作地址',
   PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `image_source`;
+CREATE TABLE `image_source`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `create_at` datetime(3) NULL DEFAULT NULL,
+  `update_at` datetime(3) NULL DEFAULT NULL,
+  `img_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片URL',
+  `img_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片名称',
+  `img_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片类型',
+  `img_size` bigint NULL DEFAULT NULL COMMENT '图片大小（字节）',
+  `img_id` binary(16) NOT NULL COMMENT '图片ID（uuid_to_bin）',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `img_id`(`img_id` ASC) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
